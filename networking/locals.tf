a926425c9cb5bc4cbd085db9f4fb4272cb9a0ab4
Giovani <giovanidejesus560@gmail.com>
2023-10-16 06:17:48 -0400

    Adding SG

---
+++ networking/locals.tf
@@ -0,0 +1,35 @@
+locals {
+    security_groups =  {
+        public = {
+            name = "public_sg"
+            description = "Security group for SSH and HTTPS"
+            ingress = {
+                ssh = {
+                    from = 22 
+                    to = 22
+                    protocol = "tcp"
+                    cidr_block = ["0.0.0.0/0"]
+                }
+                http = {
+                    from = 80 
+                    to = 80
+                    protocol = "tcp" 
+                    cidr_block = ["0.0.0.0/0"]
+                }
+            }
+        }
+        RDS = {
+        name = "RDS_sg"
+        description = "Security group for RDS"
+        ingress = {
+            custom = {
+                from = 3306
+                to = 3306
+                protocol = "tcp"
+                cidr_block = [var.vpc_cidr]
+            }
+        }
+    }
+    }
+    
+}
